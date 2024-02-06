50c5af0f57653238b3f6b0afffef6aa72c2f7d39
austin1610 <austinsparks09@gmail.com>
2024-02-06 16:24:06 -0600

    Added delegate methods for location manager

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -32,6 +32,22 @@ class CurrentLocationViewController: UIViewController, CLLocationManagerDelegate
       kCLLocationAccuracyNearestTenMeters
         locationManager.startUpdatingLocation()
     }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(
+       _ manager: CLLocationManager,
+       didFailWithError error: Error
+    ) {
+        print("didFailWithError \(error.localizedDescription)")
+    }
+    
+    func locationManager(
+       _ manager: CLLocationManager,
+       didUpdateLocations locations: [CLLocation]
+    ) {
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+    }
 
 
 }
