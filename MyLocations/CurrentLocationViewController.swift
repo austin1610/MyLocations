61a50ce2aa887de8eb1982627114867c1ab48737
austin1610 <austinsparks09@gmail.com>
2024-02-06 15:18:36 -0600

    Added outlet variables and getLocation method to CurrentLocationVC

--- MyLocations/CurrentLocationViewController.swift
+++ MyLocations/CurrentLocationViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class CurrentLocationViewController: UIViewController {
+    
+    // All needed outlets
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Action
+    @IBAction func getLocation() {
+        
     }
 
 
