237841cdee8a6eef9d0176cdebfe469ac02a2eeb
austin1610 <austinsparks09@gmail.com>
2024-02-07 14:59:18 -0600

    Added new methods

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 import CoreLocation
 
+private let dateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .short
+    return formatter
+}()
+
 class LocationDetailsViewController: UITableViewController {
     @IBOutlet var descriptionTextView: UITextView!
     @IBOutlet var categoryLabel: UILabel!
@@ -21,6 +28,24 @@ class LocationDetailsViewController: UITableViewController {
         longitude: 0)
     var placemark: CLPlacemark?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        descriptionTextView.text = ""
+        categoryLabel.text = ""
+        latitudeLabel.text = String(
+            format: "%.8f",
+            coordinate.latitude)
+        longitudeLabel.text = String(
+            format: "%.8f",
+            coordinate.longitude)
+        if let placemark = placemark {
+            addressLabel.text = string(from: placemark)
+        } else {
+            addressLabel.text = "No Address Found"
+        }
+        dateLabel.text = format(date: Date())
+    }
+    
  // MARK: - Actions
  @IBAction func done() {
      navigationController?.popViewController(animated: true)
@@ -28,4 +53,28 @@ class LocationDetailsViewController: UITableViewController {
  @IBAction func cancel() {
      navigationController?.popViewController(animated: true)
  }
+}
+
+// MARK: - Helper Methods
+func string(from placemark: CLPlacemark) -> String {
+    var text = ""
+    if let tmp = placemark.subThoroughfare {
+        text += tmp + " "
+    }
+    if let tmp = placemark.thoroughfare {
+        text += tmp + ", "
+    }
+    if let tmp = placemark.locality {
+        text += tmp + ", "
+    }
+    if let tmp = placemark.administrativeArea {
+        text += tmp + " "
+    }
+    if let tmp = placemark.postalCode {
+        text += tmp + ", "
+    }
+    if let tmp = placemark.country {
+        text += tmp
+    }
+    return text
 }
