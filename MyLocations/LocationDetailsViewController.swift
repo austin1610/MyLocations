b324d832e2ba82b35ea1ccbb7d865e45d1cdabf4
austin1610 <austinsparks09@gmail.com>
2024-02-07 00:13:13 -0600

    Added outlets and actions to new view controller

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -5,4 +5,20 @@
 //  Created by Austin Sparks on 2/7/24.
 //
 
-import Foundation
+import UIKit
+class LocationDetailsViewController: UITableViewController {
+    @IBOutlet var descriptionTextView: UITextView!
+    @IBOutlet var categoryLabel: UILabel!
+    @IBOutlet var latitudeLabel: UILabel!
+    @IBOutlet var longitudeLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+ // MARK: - Actions
+ @IBAction func done() {
+     navigationController?.popViewController(animated: true)
+ }
+ @IBAction func cancel() {
+     navigationController?.popViewController(animated: true)
+ }
+}
