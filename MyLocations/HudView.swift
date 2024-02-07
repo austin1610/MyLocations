eaeebd4874d8acd3926d2b535748f6ded56b24d9
austin1610 <austinsparks09@gmail.com>
2024-02-07 16:06:33 -0600

    Added needed code for HUD

--- MyLocations/HudView.swift
+++ MyLocations/HudView.swift
@@ -5,4 +5,24 @@
 //  Created by Austin Sparks on 2/7/24.
 //
 
-import Foundation
+import UIKit
+
+class HudView: UIView {
+    var text = ""
+    
+    class func hud(
+        inView view: UIView,
+        animated: Bool
+    ) -> HudView {
+        let hudView = HudView(frame: view.bounds)
+        hudView.isOpaque = false
+        view.addSubview(hudView)
+        view.isUserInteractionEnabled = false
+        hudView.backgroundColor = UIColor(
+            red: 1,
+            green: 0,
+            blue: 0,
+            alpha: 0.5)
+        return hudView
+    }
+}
