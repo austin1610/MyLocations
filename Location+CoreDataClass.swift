9f05ae9defd58c105958d90e10f744550bb1c74f
austin1610 <austinsparks09@gmail.com>
2024-02-14 12:30:32 -0600

    Added coreData code

---
+++ Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Austin Sparks on 2/14/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
