fa490d986ac7bee17300113a8e074d43fa0af27b
austin1610 <austinsparks09@gmail.com>
2024-02-14 15:16:27 -0600

    Added new method to function.swift

--- MyLocations/Functions.swift
+++ MyLocations/Functions.swift
@@ -15,3 +15,13 @@ let applicationDocumentsDirectory: URL = {
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     return paths[0]
 }()
+
+let dataSaveFailedNotification = Notification.Name(
+    rawValue: "DataSaveFailedNotification")
+
+func fatalCoreDataError(_ error: Error) {
+    print("*** Fatal error: \(error)")
+    NotificationCenter.default.post(
+    name: dataSaveFailedNotification,
+    object: nil)
+}
