c808a9ad8e762d27b8f7d066b342b356ba2aa102
austin1610 <austinsparks09@gmail.com>
2024-02-14 21:31:25 -0600

    Added a new file

---
+++ LocationCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationCellTableViewCell.swift
+//  MyLocations
+//
+//  Created by Austin Sparks on 2/14/24.
+//
+
+import UIKit
+
+class LocationCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
