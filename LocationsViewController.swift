9d87b8276721203fe0ae42a655c3a2c4278cd579
austin1610 <austinsparks09@gmail.com>
2024-02-14 22:40:36 -0600

    added editbutton to viewdidload

--- LocationsViewController.swift
+++ LocationsViewController.swift
@@ -42,6 +42,7 @@ class LocationsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         performFetch()
+        navigationItem.rightBarButtonItem = editButtonItem
     }
     
     // MARK: - Navigation
