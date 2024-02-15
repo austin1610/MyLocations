df394e36d161df0f578ea417c695f53fd5301bdd
austin1610 <austinsparks09@gmail.com>
2024-02-14 21:47:33 -0600

    Changed code in locationsVC and added code in LocationCell

--- LocationCell.swift
+++ LocationCell.swift
@@ -22,5 +22,28 @@ class LocationCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    // MARK: - Helper Method
+    func configure(for location: Location) {
+        if location.locationDescription.isEmpty {
+            descriptionLabel.text = "(No Description)"
+        } else {
+            descriptionLabel.text = location.locationDescription
+        }
+        
+        if let placemark = location.placemark {
+            var text = ""
+            if let tmp = placemark.subThoroughfare {
+                text += tmp + " "
+            }
+            if let tmp = placemark.thoroughfare {
+                text += tmp + ", "
+            }
+            if let tmp = placemark.locality {
+                text += tmp
+            }
+            addressLabel.text = text
+        } else {
+            addressLabel.text = String(format: "Lat: %.8f, Long: %.8f", location.latitude, location.longitude)
+        }
+    }
 }
