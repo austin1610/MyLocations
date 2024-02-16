aaffea7ff6a906fb2a785a298a94213cdfe57359
austin1610 <austinsparks09@gmail.com>
2024-02-15 21:09:14 -0600

    changed code inside didfinishpickingmediawithinfo

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -237,6 +237,10 @@ extension LocationDetailsViewController: UIImagePickerControllerDelegate, UINavi
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info:
     [UIImagePickerController.InfoKey: Any]
     ) {
+        image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        if let theImage = image {
+            show(image: theImage)
+        }
         dismiss(animated: true, completion: nil)
     }
     
