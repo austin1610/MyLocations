daee9da3370b0a1b9bcbaf05075ac7389a6bc533
austin1610 <austinsparks09@gmail.com>
2024-02-15 18:14:56 -0600

    added new file and code

---
+++ MapViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MapViewController.swift
+//  MyLocations
+//
+//  Created by Austin Sparks on 2/15/24.
+//
+
+import UIKit
+import MapKit
+import CoreData
+
+class MapViewController: UIViewController {
+    @IBOutlet var mapView: MKMapView!
+    
+    var managedObjectContext: NSManagedObjectContext!
+ 
+    // MARK: - Actions
+    @IBAction func showUser() {
+        let region = MKCoordinateRegion(
+            center: mapView.userLocation.coordinate,
+            latitudinalMeters: 1000,
+            longitudinalMeters: 1000)
+        mapView.setRegion(
+            mapView.regionThatFits(region),
+            animated: true)
+    }
+    
+    @IBAction func showLocations() {
+    }
+}
+
+extension MapViewController: MKMapViewDelegate {
+}
